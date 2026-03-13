e881a6198391f9922cfdbfb4f04a4da526203f63
SparksCool <Maxwell.a.head@gmail.com>
2026-03-12 21:30:52 -0400

    basic index example

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 5005;
 
@@ -9,8 +11,20 @@ const server = http.createServer((req, res) => {
     return;
   }
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>site-x is running, new and improved!</h1>');
+  // Path to your local HTML file
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+      console.error(err);
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
 server.listen(PORT, '0.0.0.0', () => {
